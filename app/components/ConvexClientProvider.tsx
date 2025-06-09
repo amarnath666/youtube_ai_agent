82763befc162eca8e9fbff7a9ea802156fa2f446
amarnath666 <amarnathdhumal2001@gmail.com>
2025-06-09 22:24:18 +0530

    first commit

---
+++ app/components/ConvexClientProvider.tsx
@@ -0,0 +1,19 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as string);
+
+export default function ConvexClientProvider({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+        <ConvexProviderWithClerk>
+            {children}
+        </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+}
