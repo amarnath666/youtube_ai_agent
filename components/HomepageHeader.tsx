623b071052c3a29af51fa04ec8ef393fa20faa23
amarnath666 <amarnathdhumal2001@gmail.com>
2025-06-18 10:34:19 +0530

    header finalised

--- components/HomepageHeader.tsx
+++ components/HomepageHeader.tsx
@@ -1,14 +1,17 @@
+import { RainbowButton } from "./magicui/rainbow-button"
+
 const HompageHeader = () => {
   return (
     <header>
-    <nav className=" border-gray-200 px-4 lg:px-[50px] pt-[25px] bg-black w-full">
+    <nav className=" border-gray-200 px-4 lg:px-[50px] pt-[25px] bg-zinc-900 w-full">
         <div className="flex flex-wrap justify-between items-center mx-auto w-full">
             <a href="https://flowbite.com" className="flex items-center">
                 {/* <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" /> */}
                 <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white mr-3">YT Notes</span>
             </a>
             <div className="flex items-center lg:order-2">
-                <a href="#" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</a>
+                {/* <a href="#" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</a> */}
+                <RainbowButton>Log in</RainbowButton>
   
                 <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
                     <span className="sr-only">Open main menu</span>
@@ -17,26 +20,26 @@ const HompageHeader = () => {
                 </button>
             </div>
             <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-                {/* <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-2 lg:mt-0">
                     <li>
-                        <a href="#" className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">Home</a>
+                        <a href="#" className="block py-1 px-4 bg-neutral-800 text-white rounded-full border border-white/5     hover:bg-neutral-800  " aria-current="page">Home</a>
                     </li>
                     <li>
-                        <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Company</a>
+                        <a href="#" className="block border-white/5  text-gray-700   hover:bg-gray-50 lg:hover:bg-transparent  lg:hover:text-primary-700 py-1 px-4 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">How it Works</a>
                     </li>
+                    {/* <li>
+                        <a href="#" className="block  text-gray-700  hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 py-1 px-4 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Marketplace</a>
+                    </li> */}
                     <li>
-                        <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Marketplace</a>
+                        <a href="#" className="block  text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 py-1 px-4 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Features</a>
                     </li>
+                    {/* <li>
+                        <a href="#" className="block  text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 py-1 px-4 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Team</a>
+                    </li> */}
                     <li>
-                        <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Features</a>
+                        <a href="#" className="block  text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 py-1 px-4 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
                     </li>
-                    <li>
-                        <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Team</a>
-                    </li>
-                    <li>
-                        <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
-                    </li>
-                </ul> */}
+                </ul>
             </div>
         </div>
     </nav>
--- components/ParticlesDemo.tsx
+++ components/ParticlesDemo.tsx
@@ -7,6 +7,9 @@ import { Particles } from "@/components/magicui/particles";
 import HompageHeader from "./HomepageHeader";
 import AiButton from "./ai-button";
 import WorkButton from "./WorkButton";
+import { AnimatedShinyTextDemo } from "./AnimatedTextDemo";
+import { RainbowButton } from "./magicui/rainbow-button";
+import { ShimmerButton } from "./magicui/shimmer-button";
 
 export function ParticlesDemo() {
   const { resolvedTheme } = useTheme();
@@ -17,27 +20,22 @@ export function ParticlesDemo() {
   }, [resolvedTheme]);
 
   return (
-    <div className="relative h-screen  ">
+    <div className="relative h-screen bg-zinc-900 ">
       <HompageHeader />
-      <div className=" flex  w-full flex-col items-center overflow-hidden rounded-lg  pt-[100px]">
-        <span className="pointer-events-none z-10 whitespace-pre-wrap text-center text-8xl font-semibold leading-none text-black pb-[25px]">
-          Turn{" "}
-          <img
-            src="/yotube.webp"
-            alt="YouTube"
-            className="inline h-[1.2em] mx-2"
-          />
-         YouTube Videos <br />
+      <div className=" flex  w-full flex-col items-center overflow-hidden  pt-[100px]">
+        <AnimatedShinyTextDemo />
+        <span className="pointer-events-none z-10 whitespace-pre-wrap text-center text-8xl font-semibold leading-none bg-gradient-to-br from-white from-30% to-white/40 mt-[25px]   bg-clip-text text-transparent">
+           Turn YouTube Videos <br />
           into Smart Notes
         </span>
 
-        <p className="text-[20px] leading-snug text-gray-600 text-center pb-[50px]">
+        <p className="text-[20px] leading-snug text-gray-600 text-center  pb-[25px] mt-[25px]">
           Effortlessly convert any YouTube video into clear, concise notes.
           <br />
           Perfect for learning, revising, or sharing key takeaways — all in
           seconds.
         </p>
-        <WorkButton />
+        <ShimmerButton>Get Started</ShimmerButton>
         <Particles
           className="absolute inset-0 z-0"
           quantity={100}
